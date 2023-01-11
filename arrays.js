124f8f28d8fe4af3557e84ddad4d803652d35dc1
“Sarah <sarahcatherine311@gmail.com>
2023-01-11 15:45:26 -0700

    Add 3 array methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,16 @@
 var dogs = ["Daisy", "Sadie", "Winston"];
 var numberOfToys = [4, 3, 8];
 var trainedDogs = [Daisy = true, Winston = true, Sadie = false]
+
+// This array method removes the last element in the array each time it is used.
+// This will remove the element "Winston" from the array.
+dogs.pop();
+
+// This array method removes the first element in the array each time it is used.
+//This will remove the element 4 from the array
+numberOfToys.shift()
+
+// The push method adds one or more elements to the end of an array and returns the new length of the array.
+// This will add a true element to the end of the array.
+trainedDogs.push(Ollie = true)
+
