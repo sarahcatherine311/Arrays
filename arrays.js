6e7fa1c1f4221f05234addcc93eb6e9cec1f66ea
“Sarah <sarahcatherine311@gmail.com>
2023-01-11 15:53:58 -0700

    Add info about index positions

--- arrays.js
+++ arrays.js
@@ -1,10 +1,10 @@
-var dogs = ["Daisy", "Sadie", "Winston"];
-var numberOfToys = [4, 3, 8];
+var dogs = ["Daisy", "Sadie", "Winston"]
+var numberOfToys = [4, 3, 8]
 var trainedDogs = [Daisy = true, Winston = true, Sadie = false]
 
 // This array method removes the last element in the array each time it is used.
 // This will remove the element "Winston" from the array.
-dogs.pop();
+dogs.pop()
 
 // This array method removes the first element in the array each time it is used.
 //This will remove the element 4 from the array
@@ -14,3 +14,13 @@ numberOfToys.shift()
 // This will add a true element to the end of the array.
 trainedDogs.push(Ollie = true)
 
+// Index positions correspond to the elements in an array. 
+// The first element in an array is in index position 0 and it goes
+// up numerically by 1 from there. (0,1,2,3)
+
+var friends = ["Nick", "Sira", "Megan"]
+
+// Nick is in index position 0, Megan is in position 2
+
+console.log(friends[1])
+// This command will pull up Sira
